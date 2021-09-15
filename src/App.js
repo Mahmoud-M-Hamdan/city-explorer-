8d2b6c52b96f840bec51e29e26dc51bbfab94c89
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2021-09-16 00:02:12 +0300

    gdsgds

--- src/App.js
+++ src/App.js
@@ -109,6 +109,7 @@ export class App extends Component {
           />
 
         }
+        
 
 
         {
--- src/Location.js
+++ src/Location.js
@@ -38,8 +38,14 @@ return <>
         
 this.props.movies.map(item=>{
 
+
+        
 return <>
 <h3>{item.title}</h3>
+<h3>{item.average_votes}</h3>
+<h3>{item.total_votes}</h3>
+<h3>{item.popularity}</h3>
+<h3>{item.released_on}</h3>
 
 
 </>
