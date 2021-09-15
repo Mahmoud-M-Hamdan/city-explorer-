369defd7afc10c73dc74e3cb7e57d25262e422f3
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2021-09-15 18:07:54 +0300

    ff

--- src/App.js
+++ src/App.js
@@ -1,69 +1,90 @@
-  
+
 import React, { Component } from 'react';
 import Location from './Location';
 import SearchForm from './SearchForm';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 import { Alert } from 'react-bootstrap';
 
 export class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      city_name:"",
-      type:"",
-      lat:"",
-      lon:"",
-      showData:false,
+    this.state = {
+      city_name: "",
+      country_code: "",
+      type: "",
+      lat: "",
+      lon: "",
+      showData: false,
       errorMessage: '',
-      showError:false,
-      weatherData:[]
+      showError: false,
+      weatherData: [],
+      movies: [],
+      serverError: '',
+
     }
   }
-  handleLocation=(e)=>{
-    let city_name=e.target.value;
+  handleLocation = (e) => {
+    let city_name = e.target.value;
     this.setState({
-      city_name:city_name
+      city_name: city_name
     })
   }
-  handleSubmit=(e)=>{
-   
+  handleSubmit = (e) => {
+
     console.log(`${process.env.REACT_APP_LOCATIONIQ_API_KEY}`);
     e.preventDefault();
-    let config={
-      method:"GET",
-      baseURL:`https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}`,
-     
+    let config = {
+      method: "GET",
+      baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}`,
+
     }
-    
-    
-    axios(config).then(res=>{
-      let responseData=res.data[0]
+
+
+    axios(config).then(res => {
+      let responseData = res.data[0]
       this.setState({
-        city_name:responseData.display_name,
-        lon:responseData.lon,
-        lat:responseData.lat,
-        url : `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${responseData.lat},${responseData.lon}&zoom=7&size=400x600&format=png`,
-        showData:true
+        city_name: responseData.display_name,
+        country_code: responseData.address.country_code,
+        lon: responseData.lon,
+        lat: responseData.lat,
+        url: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${responseData.lat},${responseData.lon}&zoom=7&size=400x600&format=png`,
+        showData: true
 
       })
-      
-    }).then(()=>{
-let city_name=this.state.city_name.split(',')[0]
-console.log(city_name)
-      axios.get(`http://${process.env.REACT_APP_BACKEND_URL}/weather-data?lat=${this.state.lat}&lon=${this.state.lon}&searchQuery=${city_name}`)
-      .then(res=>{
-        console.log(res.data)
-      this.setState({
-weatherData:res.data
 
-      })
-      })
-    })
-    .catch(err => { 
+    }).catch(err => { 
       this.setState({errorMessage: err.message,showError:true});
+    }).then(() => {
+      axios.get(`http://${process.env.REACT_APP_BACKEND_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}`)
+        .then(res => {
+          console.log(res.data)
+          this.setState({
+            weatherData: res.data
+
+          })
+        })
     })
+      .catch(err => {
+        this.setState({ serverError: err, showError: true });
+      })
+      .then(() => {
+        axios
+          .get(
+            `http://${process.env.REACT_APP_BACKEND_URL}/movie?country_code=${this.state.country_code.toUpperCase()}`
+          )
+          .then((res) => {
+            this.setState({
+              movies: res.data,
+            });
+          })
+      }).catch((err) => {
+        this.setState({
+          serverError: err,
+        });
+      });
 
 
 
@@ -73,37 +94,40 @@ weatherData:res.data
     return (
       <>
         <h1>Welcome to City explorer</h1>
-        <SearchForm handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} getMap={this.getMap}/>
+        <SearchForm handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} getMap={this.getMap} />
         {
-          this.state.showData&&
+          this.state.showData &&
           <Location city_name={this.state.city_name}
-                    type={this.state.type}
-                    lat={this.state.lat}
-                    lon={this.state.lon}
-                    url={this.state.url}
-                    weatherData= {this.state.weatherData}
-                 
+            type={this.state.type}
+            lat={this.state.lat}
+            lon={this.state.lon}
+            url={this.state.url}
+            weatherData={this.state.weatherData}
+            movies={this.state.movies}
+
+
           />
-       
+
         }
-        
-       
-         {
-        this.state.showError &&
-            <Alert variant="success">
+
+
+        {
+          this.state.showError &&
+          <Alert variant="success">
             <Alert.Heading>Error !!</Alert.Heading>
             <p>
-           { this.state.errorMessage} 
+              {this.state.errorMessage}
+              {this.state.serverError}
             </p>
             <hr />
             <p className="mb-0">
-              please try to fix it 
+              please try to fix it
             </p>
           </Alert>
-  }
+        }
       </>
     )
   }
 }
 
- export default App
+export default App
--- src/Location.js
+++ src/Location.js
@@ -9,7 +9,7 @@ export class Location extends Component {
       
 
 <Card style={{ width: '25rem' }}>
-  <Card.Img variant="top" src={this.props.url} alt="gjf"style={{height:'250px'}}  />
+  <Card.Img variant="top" src={this.props.url} alt="gjf"style={{height:'400px'}}  />
   <Card.Body>
     <Card.Title><h1>{this.props.city_name}</h1></Card.Title>
     <Card.Text>
@@ -17,12 +17,30 @@ export class Location extends Component {
 
                 <h3>{this.props.lat}<br/>{this.props.lon}</h3>
     </Card.Text>
+    <h2> 🌧️The Weather Data 🌞</h2> 
     {
 this.props.weatherData.map(item=>{
 
 return <>
 <h3>{item.date}</h3>
 <h3>{item.description}</h3>
+
+</>
+
+
+})
+
+
+ }
+
+ <h2> The Movies List</h2>
+    {
+        
+this.props.movies.map(item=>{
+
+return <>
+<h3>{item.title}</h3>
+
 
 </>
 
